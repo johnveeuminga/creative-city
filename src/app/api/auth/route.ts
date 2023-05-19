5726b2a446856894f851ceb57c2964c86d3b4061
John Vee Uminga <johnveeuminga@gmail.com>
2023-05-19 15:34:33 +0800

    Prisma setup.

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,3 +1,14 @@
-export async function GET() {}
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const users = await prisma.user.findMany({
+
+  });
+
+  return NextResponse.json(users, {
+    status: 200,
+  });
+}
 export async function POST() {}
 export async function DELETE() {}
